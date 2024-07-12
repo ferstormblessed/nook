02456bf18df4ecc0ac22d861bc7318f0a2dba773
stormblessed <salvadorfernando2002@outlook.com>
2024-07-12 13:37:47 +0200

    Add ImGui and ImGui-SFML libraries

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,41 @@
 #include <SFML/Graphics.hpp>
+#include <imgui-SFML.h>
+#include <imgui.h>
 #include "utilities.h"
 
 int main()
 {
-    auto window = sf::RenderWindow{ { 1920u, 1080u }, "CMake SFML Project" };
+    auto window = sf::RenderWindow{ { 1920u, 1080u }, "nook" };
     window.setFramerateLimit(144);
 
+    if (!ImGui::SFML::Init(window)) {
+        return -1;
+    }
+
+    sf::Clock clock;
     while (window.isOpen())
     {
         for (auto event = sf::Event{}; window.pollEvent(event);)
         {
+            ImGui::SFML::ProcessEvent(window, event);
+
             if (event.type == sf::Event::Closed)
             {
                 window.close();
             }
         }
 
+        ImGui::SFML::Update(window, clock.restart());
+
+        ImGui::Begin("Hello, world!");
+        ImGui::Button("click me");
+        ImGui::End();
+
         window.clear();
+        ImGui::SFML::Render(window);
         window.display();
-        print("fernando");
     }
+
+    print("fernando");
+    ImGui::SFML::Shutdown();
 }
